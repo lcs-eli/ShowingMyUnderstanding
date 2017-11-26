3540fb93b8caa256ae6bc6cb3580bcafb711a91d
Echo Li <eli@lcs-mba3249.lcs.on.ca>
2017-11-26 18:46:07 -0500

    I finish

--- ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -21,6 +21,29 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Replace this command and add your code below...
 
+
+canvas.drawRectangle(bottomLeftX: 110, bottomLeftY: 60, width: 90, height: 220)
+
+let number = random(from: 1, toButNotIncluding: 3, ）
+
+if number == 1 {
+    canvas.fillColor = Color.red
+    canvas.drawEllipse(centreX: 155, centreY: 230, width: 80, height: 80, borderWidth: 1)
+    
+} else if number == 2 {
+    canvas.fillColor = Color.green
+    canvas.drawEllipse(centreX: 155, centreY: 100, width: 80, height: 80,borderWidth: 1)
+} else if number == 3 {
+    canvas.fillColor = Color.yellow
+    canvas.drawEllipse(centreX: 155, centreY: 170, width: 80, height: 80, borderWidth: 1)
+}
+                      
+
+
+
+
+
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.

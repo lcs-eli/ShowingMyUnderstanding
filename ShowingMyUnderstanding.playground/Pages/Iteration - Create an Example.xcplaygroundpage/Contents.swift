e3a240c13f525f439448af403d04b792bc4fd66a
Echo Li <eli@lcs-mba3249.lcs.on.ca>
2017-11-27 20:45:35 -0500

    I finish the example

--- ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Iteration - Create an Example.xcplaygroundpage/Contents.swift
@@ -20,6 +20,20 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this comment and add your code below...
+for y in stride(from: 295, through: 0, by: -70){
+    canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 25, centreY: y, width: 40, height: 40, borderWidth: 1)
+    canvas.drawEllipse(centreX: 75, centreY: y, width: 40, height: 40 )
+    
+    canvas.fillColor = Color.blue
+    canvas.drawEllipse(centreX: 125, centreY: y, width: 40, height: 40)
+    canvas.drawEllipse(centreX: 175, centreY: y, width: 40, height: 40)
+    
+    canvas.fillColor = Color.green
+    canvas.drawEllipse(centreX: 225, centreY: y, width: 40, height: 40)
+    canvas.drawEllipse(centreX: 275, centreY: y, width: 40, height: 40)
+}
+
 
 /*:
  ## Template code
